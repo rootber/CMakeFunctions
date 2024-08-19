1e8481ba852764c462b9441c6fd4b1481e4e3c48
openserver <opemwrt@outlook.com>
2024-08-19 11:21:28 +0800

    删除Routine目录

--- Routine/Precompile.cmake
+++
@@ -1,8 +0,0 @@
-function(precompile ProjectName HeaderFileName type)
-    if(type STREQUAL PRIVATE)
-        set(CompileType PRIVATE)
-    elseif(type STREQUAL PUBLIC)
-        set(CompileType PUBLIC)
-    endif ()
-    target_precompile_headers(${ProjectName} ${CompileType} ${HeaderFileName} )
-endfunction()
